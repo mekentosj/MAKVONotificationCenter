b736b244bd23dd359498d6d19c99b38c5714a8b0
Martin Nygren <martin.nygren@springer.com>
2014-05-24 19:11:33 +0100

    Updated project to XCode 5 settings.

--- Tests.h
+++ Tests.h
@@ -5,8 +5,8 @@
 //  Created by Gwynne on 12/1/11.
 //
 
-#import <SenTestingKit/SenTestingKit.h>
+#import <XCTest/XCTest.h>
 
-@interface MAKVONotificationCenter_Tests : SenTestCase
+@interface MAKVONotificationCenter_Tests : XCTestCase
 
 @end
